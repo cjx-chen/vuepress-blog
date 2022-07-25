dc191520d66f3fd65687efdf85d6946276682d6b
cjx-chen <807496188@qq.com>
2022-07-25 11:34:06 +0800

    feat: 增加首页

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,22 @@
 module.exports = {
   base: "/vuepress-blog/",
   // 网站标题
-  title: 'Hello VuePress',
-  description: 'Just playing around',
+  title: '嘻嘻不想干活',
+  description: '给我个班上吧',
+  dest: './dist',
+  head: [
+    ['link', {rel: 'icon', href: '/logo.jpg'}]
+  ],
   markdown: {
     // 显示代码行号
     lineNumbers: true
+  },
+  themeConfig:{
+    sidebar: 'auto',
+    nav: [
+      {text: "主页", link: "/"      },
+      { text: "前端", link: "/frontend/"},
+      { text: "算法", link: "/algorithm/" }
+    ]
   }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,16 @@
-'# Hello VuePress' 
+---
+home: true
+herolmage: /hero.png
+heroText: 嘻嘻的学习笔记
+tagline: 还不赶紧学习！
+actionText: 马上开始→
+actionLink: /zh/guide/
+features:
+- title: 简洁至上
+  details: 以 Markdown为中心的项目结构，以最少的配置帮助你专注于写作。
+- title: Vue驱动
+  details: 享受Vue + webpack的开发体验，在 Markdown中使用Vue组件，同时可以使用Vue来开发自定义主题。
+- title: 高性能
+  details: VuePress为每个页面预渲染生成静态的HTML，同时在页面被加载的时候，将作为SPA运行。
+footer: MIT Licensed | Copyright © 2022 Jessie
+---
